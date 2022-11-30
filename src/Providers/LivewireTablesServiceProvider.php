d8a00b7d3d69880ebb4c7f7ef02adbc7659ba7b0
Aaron <aaron@neondigital.co.uk>
2022-11-30 21:50:03 +0000

    test

--- src/Providers/LivewireTablesServiceProvider.php
+++ src/Providers/LivewireTablesServiceProvider.php
@@ -4,7 +4,6 @@ namespace AaronNeonDigital\LivewireTables\Providers;
 
 use AaronNeonDigital\LivewireTables\Tables\Columns\Column;
 use AaronNeonDigital\LivewireTables\Tables\Columns\NumberColumn;
-use Faker\Core\Number;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
@@ -19,12 +18,7 @@ class LivewireTablesServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__.'/../views', 'livewire-tables');
 
-//        $components = [
-//            Column::class,
-//            NumberColumn::class,
-//        ];
-
-        Livewire::component('livewire-tables::columns.column', Column::class);
-        Livewire::component('livewire-tables::columns.number-column', NumberColumn::class);
+        Livewire::component('columns.column', Column::class);
+        Livewire::component('columns.number-column', NumberColumn::class);
     }
 }
